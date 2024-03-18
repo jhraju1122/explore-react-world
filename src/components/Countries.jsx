8e9712705b8ef024da546ef64bf91bd2de71a766
jhraju <jhraju890@gmail.com>
2024-03-19 05:12:41 +0600

    added country name dynamically and set  border

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
+import Country from "./country/Country";
+ 
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
   useEffect(()=>{
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => setCountries(data));
   }, [])
   return (
     <div>
-      <h3>countries</h3>
+      <h3>countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country key={country.cca3} country={country}></Country>)
+      }
     </div>
   );
 };
